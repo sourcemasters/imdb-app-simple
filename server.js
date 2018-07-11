8d3fc626c0b58a42828dee80bf78fa5c9a78e37d
Yuhua <yuhua.wang@salesforce.com>
2018-07-11 14:46:08 -0700

    changed city to movie; testing changes with res.send

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 const request = require('request');
 const apiKey = '07ddbfef69fb0638b4014f53c316788b';
 
-const movieDB = require('moviedb')('d3d327f7f687384a8074b41ead81a040');
+const MovieDB = require('moviedb')('d3d327f7f687384a8074b41ead81a040');
 
 
 
@@ -21,22 +21,23 @@ app.get('/', function (req, res) {
 });
 
 app.post('/', function (req, res) {
-	let city = req.body.city;
-	let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
-	
-	request(url, function (err, response, body) {
-		if(err){
-			res.render('index', {weather: null, error: 'Error, please try again'});
-		} else {
-			let weather = JSON.parse(body);
-			if(weather.main == undefined){
-				res.render('index', {weather: null, error: 'Error, please try again'});
-			} else {
-				let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
-				res.render('index', {weather: weatherText, error: null});
-			}
-		}
-	})
+	let movie = req.body.movie;
+	//let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+
+	res.send(`${movie} is pretty good, or so I've heard.`);	
+	// request(url, function (err, response, body) {
+	// 	if(err){
+	// 		res.render('index', {weather: null, error: 'Error, please try again'});
+	// 	} else {
+	// 		let weather = JSON.parse(body);
+	// 		if(weather.main == undefined){
+	// 			res.render('index', {weather: null, error: 'Error, please try again'});
+	// 		} else {
+	// 			let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+	// 			res.render('index', {weather: weatherText, error: null});
+	// 		}
+	// 	}
+	// })
 })
 
 app.listen(3000, function () {
